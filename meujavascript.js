d15b005a217e2e92b59d2f73327d9ab9502969dc
LucasOliverrrr <lucasouze@gmail.com>
2024-02-29 09:25:38 -0300

    inicio da aula

---
+++ meujavascript.js
@@ -0,0 +1,6 @@
+alert("Seja Bem Vindo!") 
+
+var valor1 = parseInt(document.getElementById("ivalor1").value);
+var valo2 = parseInt(document.getElementById("ivalor2").value);
+
+var op = document.querySelector('input[name=calc]:checked').value;
