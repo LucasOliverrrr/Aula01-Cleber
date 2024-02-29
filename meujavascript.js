776d03030e4a34bec41f45ec5f12ad0896a9db4d
LucasOliverrrr <lucasouze@gmail.com>
2024-02-29 09:25:48 -0300

    inicio da aula

--- meujavascript.js
+++ meujavascript.js
@@ -1,6 +1,16 @@
 alert("Seja Bem Vindo!") 
 
-var valor1 = parseInt(document.getElementById("ivalor1").value);
-var valo2 = parseInt(document.getElementById("ivalor2").value);
+function calculadora(){
+    var valor1 = parseInt(document.getElementById("ivalor1").value);
+    var valo2 = parseInt(document.getElementById("ivalor2").value);
 
-var op = document.querySelector('input[name=calc]:checked').value;
+    var op = document.querySelector('input[name=calc]:checked').value;
+
+    if(op == 'soma'){
+        var resposta = valor1 + valor2
+        alert("A soma é" + resposta);
+}
+    if(op == 'subtracao'){
+    var resposta = valor1 - valor2
+    alert("A Subtracao é" + resposta);
+}
